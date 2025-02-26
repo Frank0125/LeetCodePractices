c039fafc4266d89f51377a405e8beadad5b85955
Frank0125 <a01252974@tec.mx>
2025-02-26 15:22:42 -0700

    ADD: Second, better solution

--- LongestSubstringWithoutRepeatingCharacters/main.cpp
+++ LongestSubstringWithoutRepeatingCharacters/main.cpp
@@ -30,8 +30,17 @@ class Solution {
                 // }
                 //Second Try
                 //Diferent Approach
-
-                return ;
+                int ans = 0;
+                map<char, int> map;
+                //ex
+                for (int j = 0, i = 0; j < s.length(); j++) {
+                    if (map.find(s[j]) != map.end()) {
+                        i = max(map[s[j]], i);
+                    }
+                    ans = max(ans, j - i + 1);
+                    map[s[j]] = j + 1;
+                }
+                return ans;
             }
         //extra function to print the map, not required in leetcode
 };
@@ -40,14 +49,14 @@ int main() {
     try {
         Solution sol;
         string s = "abcabcbb";
+        //Expected output: 3
         cout << sol.lengthOfLongestSubstring(s) << endl;
-        //Expected output: 3
         string s2 = "bbbbb";
-        cout << sol.lengthOfLongestSubstring(s2) << endl;
         //Expected output: 1
+        cout << sol.lengthOfLongestSubstring(s2) << endl;
         string s3 = "pwwkew";
-        cout << sol.lengthOfLongestSubstring(s3) << endl;
         //Expected output: 3
+        cout << sol.lengthOfLongestSubstring(s3) << endl;
         return 0;
     }
     catch (std::exception& e) {
